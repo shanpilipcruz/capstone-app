e463e4d51fad0cf4dbc8146cab4d5c9c72eb0657
Sean Philip Cruz <30104052+shanpilipcruz@users.noreply.github.com>
2019-02-05 13:06:06 +0800

    configurations

--- app/src/main/java/com/example/capstoneproject/HomeActivity.java
+++ app/src/main/java/com/example/capstoneproject/HomeActivity.java
@@ -19,7 +19,7 @@ public class HomeActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_activity);
+        setContentView(R.layout.capstone_home_activity);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
--- app/src/main/java/com/example/capstoneproject/SpeechActivity.java
+++ app/src/main/java/com/example/capstoneproject/SpeechActivity.java
@@ -1,4 +1,10 @@
 package com.example.capstoneproject;
 
+import android.os.Bundle;
+
 public class SpeechActivity extends HomeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
